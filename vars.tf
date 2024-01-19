0f20612c872d0db81ea84c8a9246d2efb2f7f051
Carlos Castillo <carlos.castillo@redfin.com>
2024-01-19 13:16:06 -0600

    Update

--- vars.tf
+++ vars.tf
@@ -1,9 +1,9 @@
-# variable "prod_role_name" {
-#   type        = string
-#   description = "AWS Prod Account role name used for integration"
-# }
+variable "prod_role_name" {
+  type        = string
+  description = "AWS Prod Account role name used for integration"
+}
 
-# variable "prod_account" {
-#   type        = string
-#   description = "AWS Prod Account number"
-# }
+variable "prod_account" {
+  type        = string
+  description = "AWS Prod Account number"
+}
